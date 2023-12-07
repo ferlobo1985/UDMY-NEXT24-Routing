ab3c7c1777ddc467acc76a61bd5f640401cc95b5
ferlobo1985 <fernando.lobo.prez@gmail.com>
2023-12-07 03:04:20 -0300

    06-Params and Searchparams
    06-Params and Searchparams

--- src/app/users/profile/[id]/[name]/page.js
+++ src/app/users/profile/[id]/[name]/page.js
@@ -1,7 +1,9 @@
-export default function UserNamePage(){
+export default function UserNamePage({params,searchParams}){
+    // console.log(params);
+    // console.log(searchParams);
     return(
         <>
-            <h1>I am the USER NAME page</h1>
+            <h1>{params.id} is the user ID, and the name is {params.name}</h1>
         </>
     )
 }
