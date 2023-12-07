0eb42df57e24653c378d42a6d5c14d0c7fb0cbe3
ferlobo1985 <fernando.lobo.prez@gmail.com>
2023-12-07 03:57:39 -0300

    10-Standard react code
    10-Standard react code

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,12 @@
-import Link from 'next/link';
+import Nav from '@/components/nav'
 
 export default function HomePage(){
+
   return(
     <>
       <h1>I am home</h1>
       <div>
-        <ul>
-          <li>
-            <Link href="/users">Users</Link>
-          </li>
-          <li>
-            <Link href="/users/profile/66">Users 66</Link>
-          </li>
-        </ul>
-
+        <Nav/>
       </div>
     </>
   )
